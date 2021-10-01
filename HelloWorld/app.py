931a395882d6d1e637503314a8ea01a7b80fdba2
Tanner <tanner.creque@gmail.com>
2021-10-01 12:03:45 -0600

    initial commit

---
+++ HelloWorld/app.py
@@ -0,0 +1,15 @@
+#print('Hello World') #String, obviously
+#Variables are temporarily stored data. Ex:
+age = 20
+price = 19.95
+first_name = "Tanner"  #Noticing no commas or ; between variable declaration.
+is_online = True  #Booleans are the same, Capital start.
+
+print(age) #Note to self, similar syntax to JS, variable definition is basically the same.
+
+#Getting user input
+name = input("What is your name? ") #Space between ? and " is so that there is a space between what is being asked to the user, and what is inputted by the user.
+print("Hello " + name)
+
+birth_year = input("Enter your birth year: ")
+age = 2021 - birth_year
